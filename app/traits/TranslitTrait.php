19c9015faf4eb36af0502ce2ab009618efa5eb1d
phrlog <kh@linkprofit.com>
2017-06-07 15:05:59 +0300

    update translit trait, add config for remove specifications

--- app/traits/TranslitTrait.php
+++ app/traits/TranslitTrait.php
@@ -81,8 +81,7 @@ trait TranslitTrait {
      */
     public function removeSpecifications($word)
     {
-        $search = ['край', 'область', 'автономная', 'автономный', 'округ', 'ао ', ' ао', 'республика',  'Респ.', 'дер.','деревня','д.','г.','пгт.',
-            'село ', ' село', 'с.','поселок','пос.','п.','дп.','ПГТ',' с.х.','ст.','п.г.т.','мкр.','пок ', ' пок', 'т.','х.','пгт ', ' пгт','рп ', ' рп','Кр.','р.п', ' '];
+        $search = include dirname(dirname(__FILE__)) . '/config/specifications.php';
 
         return str_ireplace($search, '', $word);
     }
