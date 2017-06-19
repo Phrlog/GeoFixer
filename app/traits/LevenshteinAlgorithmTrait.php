90905b32dc6852fb8d288664a1577a19d2a80ed4
phrlog <kh@linkprofit.com>
2017-06-19 18:38:45 +0300

    code style fixes

--- app/traits/LevenshteinAlgorithmTrait.php
+++ app/traits/LevenshteinAlgorithmTrait.php
@@ -7,7 +7,8 @@ namespace GeoFixer\traits;
  *
  * @package GeoFixer\traits
  */
-trait LevenshteinAlgorithmTrait {
+trait LevenshteinAlgorithmTrait
+{
 
     public $similarity = 0;
     public $meta_similarity = 0;
@@ -19,17 +20,15 @@ trait LevenshteinAlgorithmTrait {
      *
      * @param $word
      * @param $translited_words
-     * @param int $n
+     *
      * @return bool|mixed
      */
-    public function findBestMatch($word, $translited_words, $n = 2)
+    public function findBestMatch($word, $translited_words)
     {
         $words_array = [];
-        foreach($translited_words as $russian => $translit)
-        {
-
-            if(levenshtein(metaphone($word), metaphone($translit)) < mb_strlen(metaphone($word))/$n) {
-                if(levenshtein($word, $translit) < mb_strlen($word)/$n) {
+        foreach ($translited_words as $russian => $translit) {
+            if (levenshtein(metaphone($word), metaphone($translit)) < mb_strlen(metaphone($word)) / 2) {
+                if (levenshtein($word, $translit) < mb_strlen($word) / 2) {
                     $words_array[$russian] = $translit;
                 }
             }
@@ -38,11 +37,9 @@ trait LevenshteinAlgorithmTrait {
         $most_similar = $this->findMostSimilarWords($word, $words_array);
         $this->findFinalSimilirityAndLevenshtein($most_similar, $word);
 
-
-        foreach($most_similar as $russian => $latin)
-        {
-            if(levenshtein(metaphone($latin), metaphone($word)) <= $this->meta_min_levenshtein) {
-                if(similar_text(metaphone($latin), metaphone($word)) >= $this->meta_similarity) {
+        foreach ($most_similar as $russian => $latin) {
+            if (levenshtein(metaphone($latin), metaphone($word)) <= $this->meta_min_levenshtein) {
+                if (similar_text(metaphone($latin), metaphone($word)) >= $this->meta_similarity) {
                     $meta_result[$russian] = $latin;
                 }
             }
@@ -65,8 +62,7 @@ trait LevenshteinAlgorithmTrait {
 
         $result = [];
 
-        foreach($words_array as $russian => $translit)
-        {
+        foreach ($words_array as $russian => $translit) {
             if (levenshtein($translit, $word) <= $this->min_levenshtein) {
                 if (similar_text($translit, $word) >= $this->similarity) {
                     $this->min_levenshtein = levenshtein($translit, $word);
@@ -88,12 +84,11 @@ trait LevenshteinAlgorithmTrait {
      */
     private function findFinalSimilirityAndLevenshtein($most_similar, $word)
     {
-        foreach($most_similar as $item) {
+        foreach ($most_similar as $item) {
             $this->meta_min_levenshtein = min($this->meta_min_levenshtein, levenshtein(metaphone($item), metaphone($word)));
         }
 
-        foreach($most_similar as $item)
-        {
+        foreach ($most_similar as $item) {
             if (levenshtein($item, $word) == $this->meta_min_levenshtein) {
                 $this->meta_similarity = max($this->meta_similarity, similar_text(metaphone($item), metaphone($word)));
             }
