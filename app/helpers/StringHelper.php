0a71360a33e64c238c959bbb131f11c7a0de10a6
phrlog <kh@linkprofit.com>
2017-12-01 19:23:23 +0300

    fix wordTranslit bug

--- app/helpers/StringHelper.php
+++ app/helpers/StringHelper.php
@@ -46,8 +46,8 @@ class StringHelper {
      */
     public function wordTranslit($word) {
         mb_internal_encoding("UTF-8");
-        $word = $this->removeSpecifications($word);
         $word = mb_strtolower($word);
+        $word = $this->removeSpecifications($word);
         $word = $this->removeSymbols($word);
 
         return strtr($word, $this->alphabet);
